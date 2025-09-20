f032b4b1b31213ea3d9e0200bfb4c3a998cf07ed
Shrikant Shingare <shrikantshingare77@gmail.com>
2025-09-20 11:04:24 +0530

    WebServer: Update JWT

--- WebServer/internal/auth/helpers_password_test.go
+++ WebServer/internal/auth/helpers_password_test.go
@@ -1,66 +1,111 @@
 package auth
 
 import (
-	"fmt"
 	"testing"
+	"time"
+
+	"github.com/google/uuid"
 )
 
-func TestHashPassword(t *testing.T) {
-	// Test Casets
-	testCases := []struct {
-		input string
+func TestCheckPasswordHash(t *testing.T) {
+	// First, we need to create some hashed passwords for testing
+	password1 := "correctPassword123!"
+	password2 := "anotherPassword456!"
+	hash1, _ := HashPassword(password1)
+	hash2, _ := HashPassword(password2)
+
+	tests := []struct {
+		name     string
+		password string
+		hash     string
+		wantErr  bool
 	}{
 		{
-			input: "shrikant123%",
+			name:     "Correct password",
+			password: password1,
+			hash:     hash1,
+			wantErr:  false,
 		},
 		{
-			input: "rc.pheuh.,rpgc{[}]}",
+			name:     "Incorrect password",
+			password: "wrongPassword",
+			hash:     hash1,
+			wantErr:  true,
 		},
 		{
-			input: "649844444cg.fp[+))]",
+			name:     "Password doesn't match different hash",
+			password: password1,
+			hash:     hash2,
+			wantErr:  true,
 		},
 		{
-			input: "neothu5777539{}=",
+			name:     "Empty password",
+			password: "",
+			hash:     hash1,
+			wantErr:  true,
+		},
+		{
+			name:     "Invalid hash",
+			password: password1,
+			hash:     "invalidhash",
+			wantErr:  true,
 		},
 	}
 
-	//Check for no erros and valid hash
-	for _, cas := range testCases {
-		pswd, err := HashPassword(cas.input)
-
-		if err != nil {
-			t.Errorf("Getting this error: %v", err)
-			continue
-		}
-		if pswd == "" {
-			t.Errorf("Error with converting to hash")
-			continue
-		}
-		fmt.Println(pswd)
-
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := CheckPasswordHash(tt.password, tt.hash)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CheckPasswordHash() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
 	}
 }
 
-func TestCheckPasswordHash(t *testing.t) {
-	testCases := []struct {
-		inputpassword string
-		inputHash     string
+func TestValidateJWT(t *testing.T) {
+	userID := uuid.New()
+	validToken, _ := MakeJWT(userID, "secret", time.Hour)
+
+	tests := []struct {
+		name        string
+		tokenString string
+		tokenSecret string
+		wantUserID  uuid.UUID
+		wantErr     bool
 	}{
 		{
-			inputpassword: "shrikant123%",
-			inputHash:     "$2a$10$vmO0bs18kC5ILrZ5E4x0GeOkOI.F9JBaiJ21gCB9eHyb9wxUj2PS2",
+			name:        "Valid token",
+			tokenString: validToken,
+			tokenSecret: "secret",
+			wantUserID:  userID,
+			wantErr:     false,
 		},
 		{
-			inputpassword: "rc.pheuh.,rpgc{[}]}",
-			inputHash:     "$2a$10$BySdzxW5CZkF8Gprq83Pgej6oXao7nl6veBYsw1HHwud4kYRjhGhu",
+			name:        "Invalid token",
+			tokenString: "invalid.token.string",
+			tokenSecret: "secret",
+			wantUserID:  uuid.Nil,
+			wantErr:     true,
 		},
 		{
-			inputpassword: "649844444cg.fp[+))]",
-			inputHash:     "$2a$10$DKsRo6V0h5F4XOa/57ss3OtEWJIJjKKfvLqZkyafGdXRLrEh93n6a",
+			name:        "Wrong secret",
+			tokenString: validToken,
+			tokenSecret: "wrong_secret",
+			wantUserID:  uuid.Nil,
+			wantErr:     true,
 		},
-		{
-			inputpassword: "neothu5777539{}=",
-			inputHash:     "$2a$10$NsfOm6/3.dxBPSaxcAst.O2nxdlxJpDoXabvjNL/nrpxNH3taA/0u",
-		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotUserID, err := ValidateJWT(tt.tokenString, tt.tokenSecret)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateJWT() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotUserID != tt.wantUserID {
+				t.Errorf("ValidateJWT() gotUserID = %v, want %v", gotUserID, tt.wantUserID)
+			}
+		})
 	}
 }
